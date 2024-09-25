0634c5751ed495bf9df5c064dbb095aaa7f4059d
yewintaung <yewintaung.ucsm@gmail.com>
2024-09-25 13:49:16 +0000

    fix

--- aws-secret-engine-day3/iam.tf
+++ aws-secret-engine-day3/iam.tf
@@ -35,7 +35,7 @@ data "aws_iam_policy_document" "vault_admin" {
         "iam:RemoveUserFromGroup"
     ]
     resources = [
-      "arn:aws:iam::711387102232:user/vault-*"
+      "arn:aws:iam::205930650628:user/vault-*"
     ]
   }
 }
