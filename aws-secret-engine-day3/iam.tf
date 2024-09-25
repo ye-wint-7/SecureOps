0bf9d1aa7b6845079c7f6a4c145607e7ded36f49
yewintaung <yewintaung.ucsm@gmail.com>
2024-09-25 13:10:25 +0000

    aws-secret-engine

---
+++ aws-secret-engine-day3/iam.tf
@@ -0,0 +1,48 @@
+# Create an IAM user for the Vault admin
+resource "aws_iam_user" "vault_admin" {
+  name = var.vault_admin
+  path = "/"
+
+  tags = {
+    Name = "vault-admin"
+  }
+}
+
+# Create an access key for the Vault admin user
+resource "aws_iam_access_key" "vault_admin" {
+  user = aws_iam_user.vault_admin.name
+}
+
+# Define the IAM policy document for the Vault admin user
+data "aws_iam_policy_document" "vault_admin" {
+  statement {
+    effect    = "Allow"
+    actions   = [
+        "iam:AttachUserPolicy",
+        "iam:CreateUser",
+        "iam:CreateAccessKey",
+        "iam:DeleteUser",
+        "iam:DeleteAccessKey",
+        "iam:DeleteUserPolicy",
+        "iam:DetachUserPolicy",
+        "iam:GetUser",
+        "iam:ListAccessKeys",
+        "iam:ListAttachedUserPolicies",
+        "iam:ListGroupsForUser",
+        "iam:ListUserPolicies",
+        "iam:PutUserPolicy",
+        "iam:AddUserToGroup",
+        "iam:RemoveUserFromGroup"
+    ]
+    resources = [
+      "arn:aws:iam::711387102232:user/vault-*"
+    ]
+  }
+}
+
+# Attach the defined policy to the Vault admin user
+resource "aws_iam_user_policy" "vault_admin" {
+  name   = "vault-admin-policy"
+  user   = aws_iam_user.vault_admin.name
+  policy = data.aws_iam_policy_document.vault_admin.json
+}
---
+++ aws-secret-engine-day3/main.tf
@@ -0,0 +1,27 @@
+# Configure the AWS secret backend in Vault with access and secret keys
+resource "vault_aws_secret_backend" "aws" {
+  access_key = aws_iam_access_key.vault_admin.id
+  secret_key = aws_iam_access_key.vault_admin.secret
+  region     = "ap-southeast-1"
+  path       = "aws-dev"
+  
+  # Set default and maximum lease durations for AWS credentials
+  default_lease_ttl_seconds = 900
+  max_lease_ttl_seconds     = 1500
+}
+
+# Define a role within the AWS secret backend that grants admin access
+resource "vault_aws_secret_backend_role" "role" {
+  backend         = vault_aws_secret_backend.aws.path
+  name            = "admin-access-role"
+  credential_type = "iam_user"
+  
+  # Attach the AdministratorAccess policy to the role
+  policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+}
+
+# Fetch AWS access credentials for the defined role from Vault
+data "vault_aws_access_credentials" "creds" {
+  backend = vault_aws_secret_backend.aws.path
+  role    = vault_aws_secret_backend_role.role.name
+}
