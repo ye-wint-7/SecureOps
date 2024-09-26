36433089e0cfd571f42a63c2df19f936f653d022
yewintaung <yewintaung.ucsm@gmail.com>
2024-09-26 14:36:00 +0000

    test

--- aws-root-credential-test/main.tf
+++ aws-root-credential-test/main.tf
@@ -1,7 +1,7 @@
 # Configure the AWS secret backend in Vault with access and secret keys
 resource "vault_aws_secret_backend" "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = aws_iam_access_key.vault_admin.id
+  secret_key = aws_iam_access_key.vault_admin.secret
   region     = "ap-southeast-1"
   path       = "aws-dev"
   
