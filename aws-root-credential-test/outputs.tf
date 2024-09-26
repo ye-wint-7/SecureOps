840274bf09fb07276334ad78839867c4609ac3d7
yewintaung <yewintaung.ucsm@gmail.com>
2024-09-26 14:25:51 +0000

    fix

--- aws-root-credential-test/outputs.tf
+++ aws-root-credential-test/outputs.tf
@@ -1,7 +1,9 @@
 output "vpc_role_access_key" {
   value = data.vault_aws_access_credentials.creds.access_key
+  sensitive = true
 }
 
 output "vpc_role_secret_key" {
   value = data.vault_aws_access_credentials.creds.secret_key
+  sensitive = true
 }
